a06ddadbede3591b0678276fadc646b009cbab2f
guilherdrk <guibem20@gmail,com>
2025-08-13 12:54:41 -0300

    first commit

---
+++ src/test/java/com/rafacar/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafacar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
