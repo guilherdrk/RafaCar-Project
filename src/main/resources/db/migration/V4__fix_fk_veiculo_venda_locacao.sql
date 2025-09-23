04a6c408d01c0659f4a1fc6ca95d84facde5cff4
guilherdrk <guibem20@gmail,com>
2025-09-23 18:10:02 -0300

    feat: ajeitando o remove

--- src/main/resources/db/migration/V4__fix_fk_veiculo_venda_locacao.sql
+++ src/main/resources/db/migration/V4__fix_fk_veiculo_venda_locacao.sql
@@ -1,6 +1,40 @@
-ALTER TABLE venda DROP CONSTRAINT IF EXISTS fkiis4fkjndd3sqr42vashfew9l;
-ALTER TABLE locacao DROP CONSTRAINT IF EXISTS fkiis4fkjndd3sqr42vashfew9l;
+-- V4__fix_fk_veiculo_venda_locacao.sql
+-- Remove quaisquer FKs antigas que referenciem veiculo nas tabelas venda e locacao,
+-- e recria FKs com ON DELETE CASCADE de forma segura.
 
+-- Remove FKs em "venda" que referenciem "veiculo"
+DO $$
+DECLARE r RECORD;
+BEGIN
+  FOR r IN
+    SELECT conname
+    FROM pg_constraint
+    WHERE contype = 'f'
+      AND conrelid = 'venda'::regclass
+      AND confrelid = 'veiculo'::regclass
+  LOOP
+    EXECUTE format('ALTER TABLE venda DROP CONSTRAINT %I', r.conname);
+  END LOOP;
+END
+$$;
+
+-- Remove FKs em "locacao" que referenciem "veiculo"
+DO $$
+DECLARE r RECORD;
+BEGIN
+  FOR r IN
+    SELECT conname
+    FROM pg_constraint
+    WHERE contype = 'f'
+      AND conrelid = 'locacao'::regclass
+      AND confrelid = 'veiculo'::regclass
+  LOOP
+    EXECUTE format('ALTER TABLE locacao DROP CONSTRAINT %I', r.conname);
+  END LOOP;
+END
+$$;
+
+-- Cria as FKs com nomes definidos (será seguro porque já removemos qualquer FK pré-existente)
 ALTER TABLE venda
   ADD CONSTRAINT fk_venda_veiculo
   FOREIGN KEY (veiculo_id)
