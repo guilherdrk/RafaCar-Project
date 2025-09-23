80971b3589cdde40a86418cc0ab5f19c03ca789f
guilherdrk <guibem20@gmail,com>
2025-09-23 14:24:40 -0300

    feat: ajustando LocacaoService

---
+++ src/main/resources/db/migration/V3__veiculo_fk_cascade.sql
@@ -0,0 +1,8 @@
+ALTER TABLE locacao DROP CONSTRAINT IF EXISTS fkiis4fkjndd3sqr42vashfew9l;
+
+
+ALTER TABLE locacao
+  ADD CONSTRAINT fk_locacao_veiculo
+  FOREIGN KEY (veiculo_id)
+  REFERENCES veiculo (id)
+  ON DELETE CASCADE;
